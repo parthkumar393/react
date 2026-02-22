b0d4c0e34c8e5dbbd7923978bf49700d6aff1feb
parthkumar <parthkumar393@gmail.com>
2026-02-22 08:41:52 +0530

    react router

--- reactRouter/src/components/Github/Github.jsx
+++ reactRouter/src/components/Github/Github.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
 function Github() {
-    const {userId} = useParams()
+    const data = useLoaderData()
+    // const [data, setData]= useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/hiteshchoudhary')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    // }, [])
 
     return (
-        <div>Github followers: {userId}</div>
+        <div>Github followers: {data.followers}</div>
     )
 }
 
 export default Github
+
+export const gitInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+    return response.json()
+}
