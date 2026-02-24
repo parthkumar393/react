7ca8b6655d8bf1e211c837f9ac953c4de7e37359
parthkumar <parthkumar393@gmail.com>
2026-02-25 00:01:17 +0530

    changes commited

--- reactRouter/src/components/About/About.jsx
+++ reactRouter/src/components/About/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
                 <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                     <div className="md:5/12 lg:w-5/12">
                         <img
-                            src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
+                            src="https://miro.medium.com/v2/resize:fit:1400/1*QBhZWoIqi8XHU8RKKf_LIg.png"
                             alt="image"
                         />
                     </div>
